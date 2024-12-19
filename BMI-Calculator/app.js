558bd4e302142d69c3fa032478670b94d6f47d67
harsh <harsvardan075@gmail.com>
2024-12-19 06:23:38 +0530

    new project

---
+++ BMI-Calculator/app.js
@@ -0,0 +1,22 @@
+const main = document.querySelector(".main");
+
+main.addEventListener("submit", function(e){
+    e.preventDefault();
+    const height = parseInt(document.querySelector("#height").value);
+    const weight = parseInt(document.querySelector("#weight").value);
+    const result = document.querySelector("#result");
+    const reset = document.querySelector("#reset");
+
+    if(height === '' || height < 0 || isNaN(height)){
+        result.innerHTML = `Not a valid height${height}`
+    }else if(weight === '' || weight < 0 || isNaN(weight)){
+        result.innerHTML = `Not a valid weight${weight}`
+    } else {
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+        result.innerHTML = `<span>${bmi}</span>`
+    }
+
+    reset.addEventListener("click", function(){
+        result.innerHTML = "";
+    })
+});
