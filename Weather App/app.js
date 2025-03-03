8674e2859f241d433a735a5334c9b8ceaad2fda4
harsh <harsvardan075@gmail.com>
2025-03-03 11:20:20 +0530

    some changes

--- Weather App/app.js
+++ Weather App/app.js
@@ -6,23 +6,28 @@ const API_URL = "https://api.openweathermap.org/data/2.5/weather";
 const API_KEY = "45680b1a618f2271fb8599e9468c2b34";
 
 let weatherInfo = async () =>{
-    try{
+    
     let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`)
     let jsonRes = await response.json();
-    result = {
-      name:jsonRes.name,
-      temp:jsonRes.main.temp,
-      tempMax:jsonRes.main.temp_max,
-      tempMin:jsonRes.main.temp_min,
-      feelsLike:jsonRes.main.feels_like,
-      humidity:jsonRes.main.humidity,
-      description:jsonRes.weather[0].description,
-    };
-    console.log(result);
-    change();
-    } catch (err){
-      throw err
+
+    if(response.status === 404){
+      document.querySelector(".error").style.display = "block";
+      document.querySelector(".info").style.display = "none";
+    }else {
+      result = {
+        name:jsonRes.name,
+        temp:jsonRes.main.temp,
+        tempMax:jsonRes.main.temp_max,
+        tempMin:jsonRes.main.temp_min,
+        feelsLike:jsonRes.main.feels_like,
+        humidity:jsonRes.main.humidity,
+        description:jsonRes.weather[0].description,
+      };
+      console.log(result);
+      change();
     }
+      
+   
   };
 
 btn.addEventListener("click",function (e){
@@ -31,16 +36,18 @@ btn.addEventListener("click",function (e){
     console.log(city);
     weatherInfo ();
     input.value = "";
+    document.querySelector(".error").style.display = "none";
+    document.querySelector(".info").style.display = "block";
 });
 
 function change (){
-  document.all[14].innerText=result.name;
-  document.all[16].innerText=result.temp;
-  document.all[18].innerText=result.tempMin;
-  document.all[20].innerText=result.tempMax;
-  document.all[22].innerText=result.feelsLike;
-  document.all[24].innerText=result.humidity;
-  document.all[26].innerText=result.description;
+  document.querySelector("#span1").innerHTML=result.name;
+  document.querySelector("#span2").innerHTML=result.temp;
+  document.querySelector("#span3").innerHTML=result.tempMin;
+  document.querySelector("#span4").innerHTML=result.tempMax;
+  document.querySelector("#span5").innerHTML=result.feelsLike;
+  document.querySelector("#span6").innerHTML=result.humidity;
+  document.querySelector("#span7").innerHTML=result.description;
 };
 
 
--- Weather App/app2.js
+++
@@ -1,9 +0,0 @@
-// let cityName = document.querySelector("name");
-
-// let student = {name:"harsh",class:"B.tech",marks:75,city:"tundla"}
-// console.log(student);
-
-// function change (){
-//     let result = document.all[14].innerText=student.name;
-//     console.log(result);
-// };
